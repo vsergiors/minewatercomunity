c6e30f05eb7e6686eea4cee59539f0f74595f728
vsergiors <vsergiobusiness@gmail.com>
2026-04-03 20:57:52 +0200

    Update Supabase configuration with new URL and key

--- supabase.js
+++ supabase.js
@@ -3,8 +3,8 @@
 //  Cambia SUPABASE_URL y SUPABASE_ANON_KEY por los tuyos
 // =============================================
 
-const SUPABASE_URL  = 'https://TU_PROYECTO.supabase.co';
-const SUPABASE_ANON = 'TU_ANON_KEY';
+const SUPABASE_URL  = 'https://gkuuwuqwumoorqmqkuka.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrdXV3dXF3dW1vb3JxbXFrdWthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NjUwMDksImV4cCI6MjA5MDQ0MTAwOX0.ME8C09iSbowqcak2Bezbd4otWzTXyR3bV8kf4hfodUg';
 
 const { createClient } = supabase;
 const sb = createClient(SUPABASE_URL, SUPABASE_ANON);
